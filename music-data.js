71a3ea9f5a4c9397480333c1e541b5d3e7300cfa
Yuvraj Ab <yuvraajapte@gmail.com>
2026-07-10 12:12:21 +0530

    Fix track title casing and add new tracks
    Corrected the casing of the track title and added new tracks to the private tracklist.

--- music-data.js
+++ music-data.js
@@ -1,4 +1,3 @@
-// Powered by Yuvraj - Private Music Repository Data
 const privateTracklist = [
     {
         title: "Asaan Nhi Yahan",
@@ -10,12 +9,30 @@ const privateTracklist = [
         title: "Chahun Mai ya na",
         artist: "Ashiqui 2",
         thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
-        file: "musics/Chahun Mai ya Na.mp3"
+        file: "musics/Chahun Mai Ya Na.mp3"
     },
         {
         title: "MF Gabhru!",
         artist: "Karan Aujla",
         thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
         file: "musics/MF Gabhru!.mp3"
+    },
+    {
+        title: "7.7 Magnitude",
+        artist: "Karan Aujla",
+        thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
+        file: "musics/7.7 Magnitude.mp3"
+    },
+    {
+        title: "Boyfriend",
+        artist: "Karan Aujla",
+        thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
+        file: "musics/Boyfriend.mp3"
+    },
+    {
+        title: "For A Reason",
+        artist: "Karan Aujla",
+        thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
+        file: "musics/For A Reason.mp3"
     }
 ];
