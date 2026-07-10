0319fe1f47b16751874c996e2efa8e160e7031f7
Yuvraj Ab <yuvraajapte@gmail.com>
2026-07-10 11:54:45 +0530

    Add files via upload

---
+++ music-data.js
@@ -0,0 +1,21 @@
+// Powered by Yuvraj - Private Music Repository Data
+const privateTracklist = [
+    {
+        title: "Asaan Nhi Yahan",
+        artist: "Ashiqui 2",
+        thumb: "musics/covers/kahani-suno.jpg",
+        file: "musics/Aasaan Nhi Yahan.mp3"
+    },
+    {
+        title: "Chahun Mai ya na",
+        artist: "Ashiqui 2",
+        thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
+        file: "musics/Chahun Mai ya Na.mp3"
+    },
+        {
+        title: "MF Gabhru!",
+        artist: "Karan Aujla",
+        thumb: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=500", // Online link sample
+        file: "musics/MF Gabhru!.mp3"
+    }
+];
