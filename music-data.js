dfdb69c0f9558abfeec8575d643bc6cec211fcc5
Yuvraj Ab <yuvraajapte@gmail.com>
2026-07-13 12:26:31 +0530

    Update music-data.js

--- music-data.js
+++ music-data.js
@@ -2,48 +2,48 @@ const MUSIC_LIBRARY = [
   {
     id: "ashiqui-2",
     title: "Ashiqui 2",
-    cover: "ashiqui.png",
+    cover: "covers/ashiqui.png",
     songs: [
       {
         id: "ashiqui-2-1",
         title: "Asaan Nhi Yahan",
         artist: "Ashiqui 2",
-        thumb: "ashiqui.png",
+        thumb: "covers/ashiqui.png",
         file: "musics/Aasaan Nhi Yahan.mp3"
       },
       {
         id: "ashiqui-2-2",
         title: "Chahun Mai ya na",
         artist: "Ashiqui 2",
-        thumb: "ashiqui.png",
+        thumb: "covers/ashiqui.png",
         file: "musics/Chahun Mai Ya Naa.mp3"
       },
       {
         id: "ashiqui-2-3",
         title: "Aashiqui (The Love Theme)",
         artist: "Ashiqui 2",
-        thumb: "ashiqui.png",
+        thumb: "covers/ashiqui.png",
         file: "musics/Aashiqui (The Love Theme).mp3"
       },
       {
         id: "ashiqui-2-4",
         title: "Sunn Raha Hai (Male)",
         artist: "Ashiqui 2",
-        thumb: "ashiqui.png",
+        thumb: "covers/ashiqui.png",
         file: "musics/Sunn Raha Hai (Male).mp3"
       },
       {
         id: "ashiqui-2-5",
         title: "Tum Hi Ho",
         artist: "Ashiqui 2",
-        thumb: "ashiqui.png",
+        thumb: "covers/ashiqui.png",
         file: "musics/Tum Hi Ho.mp3"
       },
       {
         id: "ashiqui-2-6",
         title: "Milne Hai Mujhse Aayi",
         artist: "Ashiqui 2",
-        thumb: "ashiqui.png",
+        thumb: "covers/ashiqui.png",
         file: "musics/Milne Hai Mujhse Aayi.mp3"
       }
     ]
@@ -51,83 +51,83 @@ const MUSIC_LIBRARY = [
   {
     id: "p-pop-culture",
     title: "P-Pop Culture",
-    cover: "ppop.png",
+    cover: "covers/ppop.png",
     songs: [
       {
         id: "p-pop-culture-1",
         title: "MF Gabhru!",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/MF Gabhru!.mp3"
       },
       {
         id: "p-pop-culture-2",
         title: "7.7 Magnitude",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/7.7 Magnitude.mp3"
       },
       {
         id: "p-pop-culture-3",
         title: "Boyfriend",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/Boyfriend.mp3"
       },
       {
         id: "p-pop-culture-4",
         title: "For A Reason",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/For A Reason.mp3"
       },
       {
         id: "p-pop-culture-5",
         title: "Daytona",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/Daytona.mp3"
       },
       {
         id: "p-pop-culture-6",
         title: "Youre U Tho",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/Youre U Tho.mp3"
       },
       {
         id: "p-pop-culture-7",
         title: "P-Pop Cultue",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/P-POP CULTURE.mp3"
       },
       {
         id: "p-pop-culture-8",
         title: "Flip-Side (Sandys Interlude)",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/Flip-Side (Sandys Interlude).mp3"
       },
       {
         id: "p-pop-culture-9",
         title: "I Really Do...",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/I Really do....mp3"
       },
       {
         id: "p-pop-culture-10",
         title: "Ima Do My Thiiing",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/Ima Do My Thiiing.mp3"
       },
       {
         id: "p-pop-culture-11",
         title: "HIM",
         artist: "Karan Aujla",
-        thumb: "ppop.png",
+        thumb: "covers/ppop.png",
         file: "musics/HIM..mp3"
       }
     ]
@@ -135,7 +135,7 @@ const MUSIC_LIBRARY = [
   {
     id: "rockstar",
     title: "Rockstar 2011",
-    cover: "rockstar.png",
+    cover: "covers/rockstar.png",
     songs: [
       {
         id: "rockstar-1",
